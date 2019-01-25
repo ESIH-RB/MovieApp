eba37366a5aed15bd6caea7922e9f0a7b88b10d6
jeanlouisdavi30 <jeanlouisdavi30@yahoo.fr>
2019-01-25 11:07:31 -0500

    modifiaction REANDME

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+//import Search from './Components/Search'
+import Exemple_view from './Components/Exemple_view'
+import Navigation from './Navigation/Navigation'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Navigation/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent : 'center',
+  },
+});
